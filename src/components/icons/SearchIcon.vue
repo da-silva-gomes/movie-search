a0028a5184b54ac769999f4168d53c480c1b44b9
José Gomes <j.da.silva8.gomes@gmail.com>
2025-04-11 21:14:58 +0100

    Removing template components. Adjusting name of icons.

---
+++ src/components/icons/SearchIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="Search">
+      <g id="Search_2">
+        <circle id="Ellipse" cx="9.79099" cy="10.8301" r="7.31101" stroke="#E0E0E0" stroke-width="1.5" />
+        <path id="Vector " d="M15.4546 15.834L20.939 21.3179" stroke="#E0E0E0" stroke-width="1.5" stroke-linecap="round"
+          stroke-linejoin="round" />
+      </g>
+    </g>
+  </svg>
+</template>
